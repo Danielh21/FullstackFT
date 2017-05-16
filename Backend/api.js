0212c6ed0a6ff65cb04465bbe4112f24363b3e93
Daniel <danielhollmann@hotmail.com>
2017-05-16 18:42:36 +0200

    Conneted to Mlab

--- Backend/api.js
+++ Backend/api.js
@@ -2,9 +2,10 @@ import DAO from './dataAccess'
 import express from 'express'
 const router = express.Router();
 
-router.get("/hej", function(req,res){
-    var jsonString = JSON.stringify(DAO.getSomeData())
-    res.send(jsonString)
+router.get("/store", function(req,res){
+    DAO.getAllProducts(function(data){
+        res.send(data)
+    })
 })
 
 
--- Backend/dataAccess.js
+++ Backend/dataAccess.js
@@ -1,5 +1,6 @@
-
 var url = "mongodb://toDoUser:Hest123@ds119588.mlab.com:19588/todo"
+import {MongoClient} from 'mongodb'
+import assert from 'assert'
 
 class DataAccessObject{
 
@@ -7,8 +8,15 @@ class DataAccessObject{
         return url
     }
 
-    getSomeData(){
-        return {title:"this is a object!"}
+    getAllProducts(callback){
+        MongoClient.connect(url,function(err,db){
+            assert.equal(null,err)
+            db.collection('store').find({}).toArray(function(err,data){
+                assert.equal(null,err)
+                db.close()
+                callback(data)
+            })
+        })
     }
 
 }
